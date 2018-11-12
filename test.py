a92113c41eea7e08fac4cc9957c92312cbd750a1
DNibs <dnibs513@gmail.com>
2018-11-12 15:36:07 -0500

    minor adjustments, loaded dataset

--- test.py
+++ test.py
@@ -7,53 +7,25 @@
 import cupy as cp
 import numpy as np
 import time
-
+from glob import glob
 
 print(cp.cuda.Device().mem_info)
 
-test_np1 = np.random.rand(1000, 1000)
-test_np2 = np.random.rand(1000, 1000)
+test_np1 = np.random.rand(10000, 10000)
+test_np2 = np.random.rand(10000, 10000)
 
-test_cp1 = cp.random.rand(1000, 1000)
-test_cp2 = cp.random.rand(1000, 1000)
+test_cp1 = cp.random.rand(10000, 10000)
+test_cp2 = cp.random.rand(10000, 10000)
 
 t0 = time.time()
 np.matmul(test_np1, test_np2)
-np.matmul(test_np1, test_np2)
-np.matmul(test_np1, test_np2)
-np.matmul(test_np1, test_np2)
-np.matmul(test_np1, test_np2)
-np.matmul(test_np1, test_np2)
-np.matmul(test_np1, test_np2)
-np.matmul(test_np1, test_np2)
-np.matmul(test_np1, test_np2)
-np.matmul(test_np1, test_np2)
-np.matmul(test_np1, test_np2)
-np.matmul(test_np1, test_np2)
-np.matmul(test_np1, test_np2)
-np.matmul(test_np1, test_np2)
-np.matmul(test_np1, test_np2)
-np.matmul(test_np1, test_np2)
+
 t1 = time.time()
 print(t1-t0)
 
 t2 = time.time()
 cp.matmul(test_cp1, test_cp2)
-cp.matmul(test_cp1, test_cp2)
-cp.matmul(test_cp1, test_cp2)
-cp.matmul(test_cp1, test_cp2)
-cp.matmul(test_cp1, test_cp2)
-cp.matmul(test_cp1, test_cp2)
-cp.matmul(test_cp1, test_cp2)
-cp.matmul(test_cp1, test_cp2)
-cp.matmul(test_cp1, test_cp2)
-cp.matmul(test_cp1, test_cp2)
-cp.matmul(test_cp1, test_cp2)
-cp.matmul(test_cp1, test_cp2)
-cp.matmul(test_cp1, test_cp2)
-cp.matmul(test_cp1, test_cp2)
-cp.matmul(test_cp1, test_cp2)
-cp.matmul(test_cp1, test_cp2)
+
 t3 = time.time()
 print(t3-t2)
 
